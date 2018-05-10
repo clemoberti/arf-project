11f4d44fc2fc9cd58c201ae57bf31da60949255b
samutamm <samutamm@gmail.com>
2018-05-10 15:59:40 +0200

    get_dictionnary_pathes seems to work

--- test_ImageProcessing.py
+++ test_ImageProcessing.py
@@ -12,26 +12,28 @@ class TestImageProcessing(unittest.TestCase):
         image = ip.read_im('images/test_ocean.png')
         
         h = 32
-        all_patches_n = ip.get_all_patches(h,image).shape[0]
+        step = 32
+        all_patches_n = ip.get_all_patches(image, h,step).shape[0]
         
-        incomplete_patches_n = ip.get_incomplete_patches(image,h).shape[0]
+        incomplete_patches_n = ip.get_incomplete_patches(image,h,step).shape[0]
         self.assertEqual(incomplete_patches_n,0)
         
-        self.assertEqual(ip.get_dictionnary_patches(image, h).shape[0], all_patches_n)
+        self.assertEqual(ip.get_dictionnary_patches(image, h,step).shape[0], all_patches_n)
         
     def test_get_dictonary_with_one_black_box(self):
         ip = ImageProcessing()
         image = ip.read_im('images/test_ocean.png')
         
         h = 32
-        all_patches_n = ip.get_all_patches(h,image).shape[0]
+        step = 32
+        all_patches_n = ip.get_all_patches(image,h,step).shape[0]
         
         modified_image = ip.delete_rect(image, 25,25,10,10)
         
-        incomplete_patches_n = ip.get_incomplete_patches(modified_image,h).shape[0]
+        incomplete_patches_n = ip.get_incomplete_patches(modified_image,h, step).shape[0]
         self.assertNotEqual(incomplete_patches_n,0)
         
-        dictionnary_n = ip.get_dictionnary_patches(modified_image, h).shape[0]
+        dictionnary_n = ip.get_dictionnary_patches(modified_image, h, step).shape[0]
         
         self.assertEqual(incomplete_patches_n + dictionnary_n, all_patches_n)
 
